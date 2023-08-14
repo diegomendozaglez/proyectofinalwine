353a020ee56b23226aaf988b28a363c37b6deb73
diegomendozaglez <140664954+diegomendozaglez@users.noreply.github.com>
2023-08-14 15:38:00 -0600

    Update train_model.py

--- winequality_model/train/train_model.py
+++ winequality_model/train/train_model.py
@@ -1,1 +1,32 @@
+def model_evaluation(model, X_train, y_train, X_test, y_test):
+    
+    print('Starting ...')
+    
+    ss = StandardScaler()
+    X_train_ss = ss.fit_transform(X_train)
+    X_test_ss = ss.fit_transform(X_test)
+    print("Scaling process is done ...")
+    
+    print("**********************************")
+    
+    print("Model building process is started ...")
+    mod = model.fit(X_train, y_train)
+    mod_pred = model.predict(X_test)
+    print("Model creation process is done ...")
+    
+    print("**********************************")
+    print("Evaluation of the Model")
+    print("***********************************")
+    
+    print("Classification report of the Model: \n {}".format(classification_report(y_test, mod_pred)))
+    print("Confusion Matrix of the given Model: \n {}".format(confusion_matrix(y_test, mod_pred)))
+    print("Accuracy score of the Model: \n{}".format(accuracy_score(y_test, mod_pred)))
+    
+    print("Evaluation process is done ...")
+    
+    print("***********************************************")
+    
+    return mod
 
+rfc = RandomForestClassifier()
+model_evaluation(rfc, X_train, y_train, X_test, y_test)
