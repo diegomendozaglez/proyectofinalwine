b0605856792c08a7f3e916082ba95252c5f557e1
diegomendozaglez <140664954+diegomendozaglez@users.noreply.github.com>
2023-08-24 15:13:07 -0600

    Add files via upload

---
+++ winequality_model/app/train/train_model.py
@@ -0,0 +1,47 @@
+from sklearn.pipeline import Pipeline
+
+transformations_pipeline = Pipeline(
+                              [
+                                ('missing_indicator', MissingIndicator(variables=NUMERICAL_VARS)),
+                                ('median_imputation', NumericalImputer(variables=NUMERICAL_VARS_WITH_NA))
+
+
+Pipeline(steps=[('missing_indicator',MissingIndicator(variables=['fixed acidity','volatile acidity'])),
+              ('median_imputation',NumericalImputer(variables=['chlorides','free sulfur dioxide']))])
+
+
+X_train = transformations_pipeline.fit_transform(X_train)
+
+
+def model_evaluation(model, X_train, y_train, X_test, y_test):
+    
+    print('Starting ...')
+    
+    ss = StandardScaler()
+    X_train_ss = ss.fit_transform(X_train)
+    X_test_ss = ss.fit_transform(X_test)
+    print("Scaling process is done ...")
+    
+    print("**********************************")
+    
+    print("Model building process is started ...")
+    mod = model.fit(X_train, y_train)
+    mod_pred = model.predict(X_test)
+    print("Model creation process is done ...")
+    
+    print("**********************************")
+    print("Evaluation of the Model")
+    print("***********************************")
+    
+    print("Classification report of the Model: \n {}".format(classification_report(y_test, mod_pred)))
+    print("Confusion Matrix of the given Model: \n {}".format(confusion_matrix(y_test, mod_pred)))
+    print("Accuracy score of the Model: \n{}".format(accuracy_score(y_test, mod_pred)))
+    
+    print("Evaluation process is done ...")
+    
+    print("***********************************************")
+    
+    return mod
+
+rfc = RandomForestClassifier()
+model_evaluation(rfc, X_train, y_train, X_test, y_test)
