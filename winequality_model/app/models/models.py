d1bbf07de714b467358e6b81b88fb9be61adfb34
diegomendozaglez <140664954+diegomendozaglez@users.noreply.github.com>
2023-08-24 19:24:59 -0600

    Update models.py

--- winequality_model/app/models/models.py
+++ winequality_model/app/models/models.py
@@ -7,27 +7,27 @@ class wine(BaseModel):
 
     Attributes:
 
-        "fixed acidity" (float): Placeholder for "fixed acidity" attribute
-        "volatile acidity" (float): Placeholder for "volatile acidity" attribute
-        "citric acid" (float): Placeholder for "citric acid" attribute
-        "residual sugar" (float): Placeholder for "residual sugar" attribute
-        "chlorides" (float): Placeholder for "chlorides" attribute
-        "free sulfur dioxide" (float): Placeholder for "free sulfur dioxide" attribute
-        "total sulfur dioxide" (float): Placeholder for "total sulfur dioxide" attribute
-        "density" (float): Placeholder for "density" attribute
-        "pH" (float): Placeholder for "pH" attribute
-        "sulphates" (float): Placeholder for "sulphates" attribute
-        "alcohol" (float): Placeholder for "alcohol" attribute
+        fixed_acidity (float): Placeholder for fixed_acidity attribute
+        volatile_acidity (float): Placeholder for volatile_acidity attribute
+        citric_acid (float): Placeholder for citric_acid attribute
+        residual sugar (float): Placeholder for residual_sugar attribute
+        chlorides (float): Placeholder for chlorides attribute
+        free_sulfur_dioxide (float): Placeholder for free_sulfur_dioxide attribute
+        total_sulfur_dioxide (float): Placeholder for total_sulfur_dioxide attribute
+        density (float): Placeholder for density attribute
+        pH (float): Placeholder for pH attribute
+        sulphates (float): Placeholder for sulphates attribute
+        alcohol (float): Placeholder for alcohol attribute
         """
 
-    "fixed acidity": float
-    "volatile acidity": float
-    "citric acid": float
-    "residual sugar": float
-    "chlorides": float
-    "free sulfur dioxide": float
-    "total sulfur dioxide": float
-    "density": float
-    "pH": float
-    "sulphates": float
-    "alcohol": float
+    fixed_acidity": float
+    volatile_acidity": float
+    citric_acid": float
+    residual sugar": float
+    chlorides": float
+    free_sulfur_dioxide": float
+    total_sulfur_dioxide": float
+    density: float
+    pH": float
+    sulphates: float
+    alcohol: float
