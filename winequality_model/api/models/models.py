315bda0fa90a1887ece0229b0416d57a6af7a6e6
diegomendozaglez <140664954+diegomendozaglez@users.noreply.github.com>
2023-08-24 18:40:31 -0600

    Update models.py

--- winequality_model/api/models/models.py
+++ winequality_model/api/models/models.py
@@ -7,45 +7,27 @@ class Mobile(BaseModel):
 
     Attributes:
 
-        battery_power (float): Placeholder for battery_power attribute
-        blue (float): Placeholder for blue attribute
-        clock_speed (float): Placeholder for clock_speed attribute
-        dual_sim (float): Placeholder for dual_sim attribute
-        fc (float): Placeholder for fc attribute
-        four_g (float): Placeholder for four_g attribute
-        in_memory (float): Placeholder for float_memory attribute
-        m_dep (float): Placeholder for m_dep attribute
-        mobile_wt (float): Placeholder for mobile_wt attribute
-        n_cores (float): Placeholder for n_cores attribute
-        pc (float): Placeholder for pc attribute
-        px_height (float): Placeholder for px_height attribute
-        px_width (float): Placeholder for px_width attribute
-        ram (float): Placeholder for ram attribute
-        sc_h (float): Placeholder for sc_h attribute
-        sc_w (float): Placeholder for sc_w attribute
-        talk_time (float): Placeholder for talk_time attribute
-        three_g (float): Placeholder for three_g attribute
-        touch_screen (float): Placeholder for touch_screen attribute
-        wifi (float): Placeholder for wifi attribute
+        "fixed acidity" (float): Placeholder for "fixed acidity" attribute
+        "volatile acidity" (float): Placeholder for "volatile acidity" attribute
+        "citric acid" (float): Placeholder for "citric acid" attribute
+        "residual sugar" (float): Placeholder for "residual sugar" attribute
+        "chlorides" (float): Placeholder for "chlorides" attribute
+        "free sulfur dioxide" (float): Placeholder for "free sulfur dioxide" attribute
+        "total sulfur dioxide" (float): Placeholder for "total sulfur dioxide" attribute
+        "density" (float): Placeholder for "density" attribute
+        "pH" (float): Placeholder for "pH" attribute
+        "sulphates" (float): Placeholder for "sulphates" attribute
+        "alcohol" (float): Placeholder for "alcohol" attribute
         """
 
-    battery_power: float
-    blue: float
-    clock_speed: float
-    dual_sim: float
-    fc: float
-    four_g: float
-    int_memory: float
-    m_dep: float
-    mobile_wt: float
-    n_cores: float
-    pc: float
-    px_height: float
-    px_width: float
-    ram: float
-    sc_h: float
-    sc_w: float
-    talk_time: float
-    three_g: float
-    touch_screen: float
-    wifi: float
+    "fixed acidity": float
+    "volatile acidity": float
+    "citric acid": float
+    "residual sugar": float
+    "chlorides": float
+    "free sulfur dioxide": float
+    "total sulfur dioxide": float
+    "density": float
+    "pH": float
+    "sulphates": float
+    "alcohol": float
